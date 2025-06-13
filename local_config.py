3897ef59b3d177b08b93311239388ba2d7dbb51d
Nikolaus Knop <niko.knop003@gmail.com>
2025-06-13 12:15:01 +0200

    Add README

--- local_config.py
+++ local_config.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 def get_video_source():
     return VideoCapture(0) # returns default video input (e.g. Webcam)
+    # To use a video file as the input use
+    # return VideoCapture("path/to/the/video")
 
 def get_output_file_name():
     # Uncomment to record video to disk while analyzing
