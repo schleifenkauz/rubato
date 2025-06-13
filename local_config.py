5c01b5b07d5fdc4c441271778ea4cef1b0bfad45
Nikolaus Knop <niko.knop003@gmail.com>
2025-06-13 12:09:02 +0200

    Add local_config.py and comment some functions.

---
+++ local_config.py
@@ -0,0 +1,11 @@
+from cv2 import VideoCapture
+from datetime import datetime
+
+def get_video_source():
+    return VideoCapture(0) # returns default video input (e.g. Webcam)
+
+def get_output_file_name():
+    # Uncomment to record video to disk while analyzing
+    #timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    #return f"data/video{timestamp}.avi"
+    return None
